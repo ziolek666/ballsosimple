bc1391a16c64dd85633f6dd6dbb6134ce6a1b139
Staś Małolepszy <stas@mozilla.com>
2015-07-25 17:13:21 +0200

    underscore local vars

--- src/ifyoucan/game.js
+++ src/ifyoucan/game.js
@@ -12,24 +12,25 @@ export const Game = {
     this.physics.startSystem(Phaser.Physics.ARCADE);
     this.physics.arcade.gravity.y = 20;
 
-    this.basket = this.add.sprite(240, this.world.bounds.bottom - 10, 'basket');
-    this.basket.anchor.set(0.5, 1);
-    this.basket.currentPos = 2;
-    this.physics.enable(this.basket, Phaser.Physics.ARCADE);
-    this.basket.body.allowGravity = false;
-    this.basket.body.immovable = true;
-    this.basket.body.setSize(72, 13, 0, 0);
+    this._basket = this.add.sprite(
+      240, this.world.bounds.bottom - 10, 'basket');
+    this._basket.anchor.set(0.5, 1);
+    this._basket.currentPos = 2;
+    this.physics.enable(this._basket, Phaser.Physics.ARCADE);
+    this._basket.body.allowGravity = false;
+    this._basket.body.immovable = true;
+    this._basket.body.setSize(72, 13, 0, 0);
 
 
-    this.balls = this.add.group();
-    this.balls.createMultiple(30, 'ball', 0, false);
+    this._balls = this.add.group();
+    this._balls.createMultiple(30, 'ball', 0, false);
 
-    spawnBall.call(this, this.balls);
+    spawnBall.call(this, this._balls);
     this.time.events.loop(
-      Phaser.Timer.SECOND * 2, spawnBall.bind(this, this.balls));
+      Phaser.Timer.SECOND * 2, spawnBall.bind(this, this._balls));
 
-    this.healthbar = this.add.sprite(2, 2, 'healthbar');
-    this.healthbar.width = 160;
+    this._healthbar = this.add.sprite(2, 2, 'healthbar');
+    this._healthbar.width = 160;
 
     const keys = this.input.keyboard.createCursorKeys();
     keys.right.onUp.add(moveBasket.bind(this, 1));
@@ -37,8 +38,8 @@ export const Game = {
   },
 
   update: function() {
-    this.balls.forEachAlive(ball => ball.y >= 344 && pop.call(this, ball));
-    this.physics.arcade.collide(this.basket, this.balls, save, null, this);
+    this._balls.forEachAlive(ball => ball.y >= 344 && pop.call(this, ball));
+    this.physics.arcade.collide(this._basket, this._balls, save, null, this);
   },
 
   render: function() {
@@ -49,9 +50,9 @@ export const Game = {
 };
 
 function moveBasket(indexOffset) {
-  this.basket.currentPos = Phaser.Math.clamp(
-    this.basket.currentPos + indexOffset, 0, 4);
-  this.basket.x = positions[this.basket.currentPos];
+  this._basket.currentPos = Phaser.Math.clamp(
+    this._basket.currentPos + indexOffset, 0, 4);
+  this._basket.x = positions[this._basket.currentPos];
 }
 
 function spawnBall(group) {
@@ -71,7 +72,7 @@ function save(basket, ball) {
 
 function pop(ball) {
   ball.kill();
-  const newHealth = this.healthbar.width - 16;
-  this.add.tween(this.healthbar).to(
+  const newHealth = this._healthbar.width - 16;
+  this.add.tween(this._healthbar).to(
     {width: newHealth}, 300, Phaser.Easing.Linear.None, true, 0);
 }
