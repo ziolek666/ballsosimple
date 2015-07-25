081e7a27c6e2c6cfb766f67ff9e1be63553dbfc6
Staś Małolepszy <stas@mozilla.com>
2015-07-25 21:51:17 +0200

    add bitmap font

--- src/ballsosimple/popups.js
+++ src/ballsosimple/popups.js
@@ -4,17 +4,19 @@ export function initPopups() {
   this._popups = this.add.group();
 
   this._popups.createMultiple(20, 'popups', 0, false);
-  this._popups.forEach(popup => addButton.call(this, popup));
+  this._popups.forEach(popup => makePopup.call(this, popup));
 
   this.time.events.add(Phaser.Timer.SECOND * 4, showPopup, this);
 }
 
-function addButton(popup) {
+function makePopup(popup) {
+  const text = this.add.bitmapText(0, 0, 'bmp1', '', 32);
   const button = this.add.button(6, 6, 'close1', () => popup.kill());
+  popup.addChild(text);
   popup.addChild(button);
 }
 
-export function showPopup() {
+function showPopup() {
   const popup = this._popups.getFirstDead();
   if (!popup) {
     return;
@@ -24,6 +26,11 @@ export function showPopup() {
     this.rnd.between(0, 100),
     this.rnd.between(0, 200));
   popup.frame = this.rnd.between(0, 1);
+
+  const text = popup.children[0];
+  text.text = 'DESIRE TO CRAVE\nCall us now!';
+  text.x = 145;
+  text.y = 50;
 
   this.time.events.add(Phaser.Timer.SECOND * 2, showPopup, this);
 }
--- src/ballsosimple/preloader.js
+++ src/ballsosimple/preloader.js
@@ -7,7 +7,10 @@ export const Preloader = {
     this.load.image('ball', '../assets/ball.png');
     this.load.image('healthbar', '../assets/healthbar.png');
     this.load.image('close1', '../assets/close1.png');
-    this.load.spritesheet('popups', '../assets/popups.png', 360, 240);
+    this.load.spritesheet('popups', '../assets/popups.png', 360, 180);
+
+    this.load.bitmapFont(
+      'bmp1', 'assets/fonts/bmp1.png', 'assets/fonts/bmp1.fnt');
   },
 
   create: function() {
