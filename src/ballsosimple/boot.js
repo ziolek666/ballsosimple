4b8159e24173bc283e90b4f7831b514dcc5d6be3
Staś Małolepszy <stas@mozilla.com>
2015-07-26 01:47:42 +0200

    setScreenSize

--- src/ballsosimple/boot.js
+++ src/ballsosimple/boot.js
@@ -11,6 +11,7 @@ export const Boot = {
     this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.game.scale.pageAlignHorizontally = true;
     this.game.scale.pageAlignVertically = true;
+    this.game.scale.setScreenSize(true);
     this.game.state.start('preloader');
   }
 
