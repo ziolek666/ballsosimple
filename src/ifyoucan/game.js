35134e2506e14f6f423e55cd6ddf28ab2aaa3943
Staś Małolepszy <stas@mozilla.com>
2015-07-25 15:18:41 +0200

    spawn multiple balls

--- src/ifyoucan/game.js
+++ src/ifyoucan/game.js
@@ -9,10 +9,19 @@ export const Game = {
   },
 
   create: function() {
-    this.ball = this.add.sprite(0, 0, 'ball');
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.physics.arcade.gravity.y = 20;
+
     this.basket = this.add.sprite(240, this.world.bounds.bottom - 10, 'basket');
     this.basket.anchor.set(0.5, 1);
     this.basket.currentPos = 2;
+
+    this.balls = this.add.group();
+    this.balls.createMultiple(30, 'ball', 0, false);
+
+    spawnBall.call(this, this.balls);
+    this.time.events.loop(
+      Phaser.Timer.SECOND * 2, spawnBall.bind(this, this.balls));
 
     const keys = this.input.keyboard.createCursorKeys();
     keys.right.onUp.add(moveBasket.bind(this, 1));
@@ -20,20 +29,25 @@ export const Game = {
   },
 
   update: function() {
-    this.ball.y += 1;
+    this.balls.forEachAlive(ball =>
+      ball.y >= 344 && ball.kill());
   }
 
 };
 
 function moveBasket(indexOffset) {
-  let newPos = this.basket.currentPos + indexOffset;
-  if (newPos > 4) {
-    newPos = 4;
-  }
-  if (newPos < 0) {
-    newPos = 0;
+  this.basket.currentPos = Phaser.Math.clamp(
+    this.basket.currentPos + indexOffset, 0, 4);
+  this.basket.x = positions[this.basket.currentPos];
+}
+
+function spawnBall(group) {
+  const ball = group.getFirstDead();
+  if (!ball) {
+    return;
   }
 
-  this.basket.currentPos = newPos;
-  this.basket.x = positions[this.basket.currentPos];
+  this.physics.enable(ball, Phaser.Physics.ARCADE);
+  ball.anchor.set(0.5);
+  ball.reset(positions[this.rnd.integerInRange(0, 4)], 0 - 24);
 }
