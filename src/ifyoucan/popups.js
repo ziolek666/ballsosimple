68f4945465e4293d8a4c26bfee0848d805e82f48
Staś Małolepszy <stas@mozilla.com>
2015-07-25 18:48:47 +0200

    add button which closes popups

--- src/ifyoucan/popups.js
+++ src/ifyoucan/popups.js
@@ -2,8 +2,16 @@
 
 export function initPopups() {
   this._popups = this.add.group();
+
   this._popups.createMultiple(20, 'popup1', 0, false);
+  this._popups.forEach(popup => addButton.call(this, popup));
+
   this.time.events.add(Phaser.Timer.SECOND * 4, showPopup, this);
+}
+
+function addButton(popup) {
+  const button = this.add.button(6, 6, 'close1', () => popup.kill());
+  popup.addChild(button);
 }
 
 export function showPopup() {
@@ -12,8 +20,7 @@ export function showPopup() {
     return;
   }
 
-  popup.anchor.set(0.5);
-  popup.reset(200, 200);
+  popup.reset(20, 60);
 
   this.time.events.add(Phaser.Timer.SECOND * 2, showPopup, this);
 }
