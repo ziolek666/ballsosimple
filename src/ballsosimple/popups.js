0974b7f8275019f4e0544b5b843f925cc9d1b13e
Staś Małolepszy <stas@mozilla.com>
2015-07-26 03:07:52 +0200

    fix getFirstDead

--- src/ballsosimple/popups.js
+++ src/ballsosimple/popups.js
@@ -5,12 +5,12 @@ import { ads, NUM_ADS } from './ads';
 export function initPopups() {
   this._popups = this.add.group();
 
-  this._popups.createMultiple(50, 'popups', 0, false);
+  this._popups.createMultiple(100, 'popups', 0, false);
 
   let i = this.rnd.between(0, NUM_ADS);
   this._popups.forEach(popup => makePopup.call(this, popup, i++ % NUM_ADS));
 
-  this.time.events.add(Phaser.Timer.SECOND * 15, showPopup, this);
+  this.time.events.add(Phaser.Timer.SECOND * 10, showPopup, this);
 }
 
 function makePopup(popup, i) {
@@ -24,15 +24,22 @@ function makePopup(popup, i) {
 }
 
 function showPopup() {
-  const popup = this._popups.getFirstDead();
-  if (!popup) {
-    return;
-  }
+  let i = 0;
+  let popup;
+  do {
+    popup = this._popups.getRandom();
+  } while (popup.alive && i++ < 10);
 
   popup.reset(
     this.rnd.between(0, 100),
     this.rnd.between(0, 200));
   popup.animations.play('idle');
+  popup.bringToTop();
 
-  this.time.events.add(Phaser.Timer.SECOND * 3, showPopup, this);
+  this.time.events.add(Phaser.Timer.SECOND * delay.call(this), showPopup, this);
+}
+
+function delay() {
+  const elapsed = Math.min(this._timeElapsed, 120);
+  return Phaser.Math.mapLinear(elapsed, 0, 120, 6, 0.1);
 }
